0163b32fcdf9f016322bbb79facb1d8aecc7acad
iamrosada0 <luisrosada@outlook.com>
2025-05-22 11:20:33 +0100

    feat: add geio ip

--- internal/ads/server.go
+++ internal/ads/server.go
@@ -1,18 +1,52 @@
 package ads
 
-import "github.com/valyala/fasthttp"
+import (
+	"log"
+	"net"
+	"net/http"
+
+	realip "github.com/ferluci/fast-realip"
+	"github.com/mssola/user_agent"
+	"github.com/oschwald/geoip2-golang"
+	"github.com/valyala/fasthttp"
+)
 
 type Server struct {
+	geoip *geoip2.Reader
 }
 
-func NewServer() *Server {
-	return &Server{}
+func NewServer(geoip *geoip2.Reader) *Server {
+	return &Server{geoip: geoip}
 }
 
 func (s *Server) Listen() error {
-	return fasthttp.ListenAndServe(":8080", s.handleHttp)
+	return fasthttp.ListenAndServe(":8080", s.handler)
 }
 
-func (s *Server) handleHttp(ctx *fasthttp.RequestCtx) {
-	ctx.WriteString("Hello, world")
+func (s *Server) handler(ctx *fasthttp.RequestCtx) {
+	remoteIp := realip.FromRequest(ctx)
+	ua := string(ctx.Request.Header.UserAgent())
+
+	parsed := user_agent.New(ua)
+	browserName, _ := parsed.Browser()
+
+	country, err := s.geoip.Country(net.ParseIP(remoteIp))
+	if err != nil {
+		log.Printf("Failed to parse country: %v", err)
+		return
+	}
+
+	user := &User{
+		Country: country.Country.IsoCode,
+		Browser: browserName,
+	}
+	campaigns := GetStaticCampaigns()
+
+	winner := MakeAuction(campaigns, user)
+	if winner == nil {
+		ctx.Redirect("https://example.com", http.StatusSeeOther)
+		return
+	}
+
+	ctx.Redirect(winner.ClickUrl, http.StatusSeeOther)
 }
